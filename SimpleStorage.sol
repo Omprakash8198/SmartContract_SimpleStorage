9b96dbd75b964c7a75a18ec8d584574e42841488
Omprakash Sah <47747908+Omprakash8198@users.noreply.github.com>
2020-04-28 14:42:04 +0530

    Add files via upload

---
+++ SimpleStorage.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.0 <0.6.0;
+
+
+contract SimpleStorage{
+    uint storedData;
+    
+    function set(uint x)public{
+        storedData = x;
+        
+    }
+    
+    function get() constant public returns(uint){
+        return storedData;
+    }
+    
+    function increment(uint n) public{
+        storedData+=n;
+        return;
+    }
+    
+    function decrement(uint n)public{
+        storedData-=n;
+        return;
+    }
+}
